1c1c2d0db6393923c13268816909d9ad5aa96abc
William <contjbs@msn.com>
2017-11-29 20:25:34 -0200

    Correcao de teste 2

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,10 @@ var expect = require('chai').expect;
 var index = require('../index.js');
 
 describe('Testando a aplicação', function(){
-    it('Igualdade entre objetos', function(){
+    it('Teste de Igualdade entre objetos com sucesso.', function(){
         expect(true).to.be.equal(index.isEqual(2,2));
+    });
+    it('Teste de Igualdade entre objetos com falha', function(){
+        expect(true).to.be.equal(index.isEqual(2,3));
     });
 });

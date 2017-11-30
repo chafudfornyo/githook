5baaca87bc982d4bbd9e750d535fa2d6987ba970
William <contjbs@msn.com>
2017-11-30 19:59:14 -0200

    Correcao 2

---
+++ index.js
@@ -0,0 +1,6 @@
+'user strict'
+var _ = require('lodash');
+
+module.exports.isEqual = function (obj_1, obj_2){
+    return _.isEqual(obj_1, obj_2);
+}
---
+++ test/index.test.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+var index = require('../index.js');
+
+describe('Testando a aplicação', function(){
+    it('Teste de Igualdade entre objetos com sucesso.', function(){
+        expect(true).to.be.equal(index.isEqual(2,2));
+    });
+    it('Teste de Igualdade entre objetos com falha', function(){
+        expect(false).to.be.equal(index.isEqual(2,3));
+    });
+    it('teste commit',function(){
+    });
+});

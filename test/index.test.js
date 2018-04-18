97e0007933fe7e42f502471586e81102965a133c
William <contjbs@msn.com>
2018-04-18 14:33:46 -0300

    Testando VS Code

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,7 @@ var index = require('../index.js');
 describe('Testando a aplicação', function(){
     it('Teste de Igualdade entre objetos com sucesso.', function(){
         expect(true).to.be.equal(index.isEqual(2,2));
+        expect(true).to.be.equal(index.isEqual(2,2));
     });
     it('Teste de Igualdade entre objetos com falha', function(){
         expect(false).to.be.equal(index.isEqual(2,3));
